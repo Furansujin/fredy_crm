50d4ecc657490964a40bd08b059f2bc0941e7f8d
HSSAINI Saad <hssainisaad@gmail.com>
2025-05-09 06:21:17 +0800

    init

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,19 +2,20 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
 import proxyOptions from './proxyOptions';
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tailwindcss()],
+	resolve: {
+	  alias: {
+		"@": path.resolve(__dirname, "./src"),
+	  },
+	},
 	server: {
 		port: 8080,
 		host: '0.0.0.0',
 		proxy: proxyOptions
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, 'src')
-		}
 	},
 	build: {
 		outDir: '../fredy_crm/public/frontend',
