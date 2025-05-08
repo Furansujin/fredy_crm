774469ac72cc667787a378117149d5ec69c59599
HSSAINI Saad <hssainisaad@gmail.com>
2025-05-09 05:55:06 +0800

    init

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import path from 'path';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
+import proxyOptions from './proxyOptions';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	server: {
+		port: 8080,
+		host: '0.0.0.0',
+		proxy: proxyOptions
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src')
+		}
+	},
+	build: {
+		outDir: '../fredy_crm/public/frontend',
+		emptyOutDir: true,
+		target: 'es2015',
+	},
+});
